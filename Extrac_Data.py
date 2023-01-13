34612104b1041e5a920cfcd1c8023977626db9f4
oy0rzabal <oy0rzabal.l0pez@gmail.com>
2023-01-13 00:50:12 -0600

    comit

--- Extrac_Data.py
+++ Extrac_Data.py
@@ -3,4 +3,17 @@ import random
 from time from sleep
 from selenium import webdriver
 
-#
+#controlamos el webdriver:
+def setup(self):
+    self.driver = webdriver.Chrome('./chromediver.exe')
+
+def test_search():
+    driver = self.driver
+    driver.get("https://www.inmuebles24.com/oficinas-en-venta-q-acapulco.htmlv") #Mandame a llamar la pagina
+    casas = driver.find_elemtns_by_xpath()
+
+
+def tearDown():
+    self.driver.close()
+
+if __name__ == '__main__':

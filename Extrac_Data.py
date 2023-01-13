aa5eca2a3fd586791fe470027ffd0bafd940b334
oy0rzabal <oy0rzabal.l0pez@gmail.com>
2023-01-13 01:40:07 -0600

    comit

--- Extrac_Data.py
+++ Extrac_Data.py
@@ -4,25 +4,16 @@ from time import sleep
 from selenium import webdriver
 
 #controlamos el webdriver:
-def setup(self):
-    self.driver = webdriver.Chrome('./chromediver.exe')
 
-def test_search(self):
-    driver = self.driver
-    driver.get("https://www.inmuebles24.com/oficinas-en-venta-q-acapulco.htmlv") #Mandame a llamar la pagina
-    self.casas = driver.find_elemtns_by_xpath('//div[@class="sc-1tt2vbg-3 kGTsiT"]')
+driver = webdriver.Chrome('./chromediver.exe')
 
-def extrac_data_test(sel):
-    casa = self.casas
-    for i in casa:
-        precio = casa.find_element_by_xpath('.//div[@class="sc-12dh9kl-3 gGCVnu"]').text
-        localizacion = casa.find_element_by_xpath('//div[@class="sc-ge2uzh-2 ggYRKF"]').text
-        zona = casa.find_element_by_xpath('//div[@class ="sc-ge2uzh-0 lnBueA"]').text
-        m2_estacionamiento = casa.find_element_by_xpath('//div[@class="sc-1uhtbxc-0 cAEjmC"]').text
-        direccion = casa.fins_element_by_spath('//div[@class="sc-i1odl-12 bKAWqU"]').text
+driver = driver
+driver.get("https://www.inmuebles24.com/oficinas-en-venta-q-acapulco.htmlv") #Mandame a llamar la pagina
+casa = driver.find_elemtns_by_xpath('//div[@class="sc-1tt2vbg-3 kGTsiT"]')
 
-def tearDown():
-    self.driver.close()
-
-if __name__ == '__main__':
-    extrac_data_test()
+for i in casa:
+    precio = casa.find_element_by_xpath('.//div[@class="sc-12dh9kl-3 gGCVnu"]').text
+    localizacion = casa.find_element_by_xpath('//div[@class="sc-ge2uzh-2 ggYRKF"]').text
+    zona = casa.find_element_by_xpath('//div[@class ="sc-ge2uzh-0 lnBueA"]').text
+    m2_estacionamiento = casa.find_element_by_xpath('//div[@class="sc-1uhtbxc-0 cAEjmC"]').text
+    direccion = casa.fins_element_by_spath('//div[@class="sc-i1odl-12 bKAWqU"]').text

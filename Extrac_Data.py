7b497752f99082d43d0f36c25ad3938d62165164
oy0rzabal <oy0rzabal.l0pez@gmail.com>
2023-01-13 01:10:21 -0600

    commint

--- Extrac_Data.py
+++ Extrac_Data.py
@@ -7,13 +7,22 @@ from selenium import webdriver
 def setup(self):
     self.driver = webdriver.Chrome('./chromediver.exe')
 
-def test_search():
+def test_search(self):
     driver = self.driver
     driver.get("https://www.inmuebles24.com/oficinas-en-venta-q-acapulco.htmlv") #Mandame a llamar la pagina
-    casas = driver.find_elemtns_by_xpath()
+    self.casas = driver.find_elemtns_by_xpath('//div[@class="sc-1tt2vbg-3 kGTsiT"]')
 
+def extrac_data_test(sel):
+    casa = self.casas
+    for i in casa:
+        precio = self.casas.find_element_by_xpath('.//div[@class="sc-12dh9kl-3 gGCVnu"]')
+        localizacion = self.casas.find_element_by_xpath('//div[@class="sc-ge2uzh-2 ggYRKF"]')
+        zona = self.casas.find_element_by_xpath('//div[@class ="sc-ge2uzh-0 lnBueA"]')
+        m2_estacionamiento = self.casas.find_element_by_xpath('//div[@class="sc-1uhtbxc-0 cAEjmC"]')
+        direccion = self.casas.fins_element_by_spath('//div[@class="sc-i1odl-12 bKAWqU"]')
 
 def tearDown():
     self.driver.close()
 
 if __name__ == '__main__':
+    extrac_data_test()
